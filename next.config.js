42d41060c75994b5dc9faf9c734ec9f9f4eaa8da
kimo2121 <kareem.gaad@gmail.com>
2022-07-20 20:26:20 +0200

    more modifications

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
-// next.config.js
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
   images: {
     domains: ["images.unsplash.com", "i.ytimg.com"],
   },
 };
+
+module.exports = nextConfig;
